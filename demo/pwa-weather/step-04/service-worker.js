84b3bd7cd7f2e6e7bd3261e120bcf8e661a22e56
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-07 22:05:11 +0100

    adds service-worker.js to step-04

---
+++ demo/pwa-weather/step-04/service-worker.js
@@ -0,0 +1,12 @@
+var cacheName = 'weatherPWA-step-5-1';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
