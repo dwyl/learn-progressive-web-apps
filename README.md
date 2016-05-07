4dbc996c8e53c9d779829dbe06adc6d14e52cc87
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-07 22:01:54 +0100

    run the demo with python -m SimpleHTTPServer 8000

--- README.md
+++ README.md
@@ -14,9 +14,14 @@ as single-page apps which manage stare on the client the client.
 
 > Tutorial: https://developers.google.com/web/fundamentals/getting-started/your-first-progressive-web-app/
 
+Run it with `python -m SimpleHTTPServer 3000` then visit: http://localhost:3000/demo/pwa-weather/
 
 
 ## Background Reading
 
 + Progressive enhancement is still important:
 https://jakearchibald.com/2013/progressive-enhancement-still-important/
++ Introduction to Service Workers:
+http://www.html5rocks.com/en/tutorials/service-worker/introduction/
++ Can we *use* Service Workers?
+http://caniuse.com/#feat=serviceworkers 53% at time of writing (***No IOS***)
--- demo/pwa-weather/resources/step3/app.js
+++ demo/pwa-weather/resources/step3/app.js
@@ -39,6 +39,7 @@
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
     app.toggleAddDialog(false);
+    app.saveSelectedCities();
   });
 
   document.getElementById('butAddCancel').addEventListener('click', function() {
@@ -149,6 +150,33 @@
     });
   };
 
+  var initialWeatherForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
   var fakeForecast = {
     key: 'newyork',
     label: 'New York, NY',
@@ -176,6 +204,27 @@
     }
   };
   // Uncomment the line below to test with the provided fake data
-  // app.updateForecastCard(fakeForecast);
+  app.updateForecastCard(fakeForecast);
+
+  // Save list of cities to localStorage, see note below about localStorage.
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    // IMPORTANT: See notes about use of localStorage.
+    localStorage.selectedCities = selectedCities;
+  };
+
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
 
 })();
