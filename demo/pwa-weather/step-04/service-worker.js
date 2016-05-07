033b501496f7572cf194a39bd9bf64470dc3b46d
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-07 22:29:58 +0100

    adds screenshot of step 4 to readme

--- demo/pwa-weather/step-04/service-worker.js
+++ demo/pwa-weather/step-04/service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log('[ServiceWorker] Caching app shell', cache);
       return cache.addAll(filesToCache);
     })
   );
